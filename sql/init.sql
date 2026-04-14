1ee70511ab0477913cf95ca9d142daf24bf1ebfd
MatSosa1 <dmsosa3@espe.edu.ec>
2026-04-14 12:36:12 -0500

    init.sql to PSQL

--- sql/init.sql
+++ sql/init.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE RESERVASHOTEL;
 
 CREATE TABLE HOTEL (
-    hotel_id INT AUTO_INCREMENT PRIMARY KEY,
+    hotel_id SERIAL PRIMARY KEY,
     hotel_name VARCHAR(100) NOT NULL,
     hotel_direction VARCHAR(255) NOT NULL,
     hotel_stars INT,
@@ -9,19 +9,19 @@ CREATE TABLE HOTEL (
 );
 
 CREATE TABLE CLIENTE (
-    cliente_id INT AUTO_INCREMENT PRIMARY KEY,
+    cliente_id SERIAL PRIMARY KEY,
     cliente_name VARCHAR(100) NOT NULL,
     cliente_email VARCHAR(100) UNIQUE NOT NULL,
     cliente_phone VARCHAR(100)
 );
 
 CREATE TABLE RESERVA (
-    reserva_id INT AUTO_INCREMENT PRIMARY KEY,
-    reserva_date_entrada DATETIME NOT NULL,
-    reserva_date_salida DATETIME NOT NULL,
+    reserva_id SERIAL PRIMARY KEY,
+    reserva_date_entrada TIMESTAMP NOT NULL,
+    reserva_date_salida TIMESTAMP NOT NULL,
     reserva_num_huespedes INT DEFAULT 1,
     hotel_id INT NOT NULL,
     cliente_id INT NOT NULL,
-    FOREIGN KEY (hotel_id) REFERENCES HOTEL(hotel_id),
-    FOREIGN KEY (cliente_id) REFERENCES CLIENTE(cliente_id)
+    FOREIGN KEY (hotel_id) REFERENCES hotel(hotel_id),
+    FOREIGN KEY (cliente_id) REFERENCES cliente(cliente_id)
 );
